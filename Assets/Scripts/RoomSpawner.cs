05f42edc0e7c6d26f64f770549cbf2242a7141b0
26689 <tasso.mevissen@gmail.com>
2020-10-20 14:04:05 +0200

    Initial commit

---
+++ Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 ----> need bottom door
+    // 2 ----> need top door
+    // 3 ----> need left door
+    // 4 ----> need right door
+
+
+     void Update()
+    {
+        if (openingDirection == 1)
+        {
+            // need to spawn a room with a Bottom door
+        }
+        else if (openingDirection ==2)
+        {
+            // need to spawn a room with a Top door
+        }
+        else if (openingDirection == 3)
+        {
+            // need to spawn a room with a left door
+        }
+        else if (openingDirection == 4)
+        {
+            // need to spawn a room with a right door
+        }
+    }
+}
